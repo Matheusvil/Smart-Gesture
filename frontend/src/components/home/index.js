fee96e5c544bd5c991069e5bcc395a69a21e3594
Matheusvil <matheusvilela65@hotmail.com>
2021-03-06 11:59:03 -0300

    :lipstick::construction::heavy_plus_sign:Added react-player, react-webcam and TensorFlow dependency into the project

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
+import ReactPlayer from 'react-player';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
 import './style.scss';
 
 const Home = () =>{
     return(
-        <h1>Conteudo Aqui</h1>
+        <div className='player-wrapper'>
+            <ReactPlayer className='react-player' url={['https://www.youtube.com/watch?v=M7TKRcA8KDk', 'https://www.youtube.com/watch?v=ilkgp4bA4Bc']} controls={true} width = '75%' height = '75%'/>
+            <Webcam className='webcam-display'/>
+        </div>
     );
 }
 
